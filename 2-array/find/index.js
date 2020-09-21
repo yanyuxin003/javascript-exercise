84675382bbe7acff2bd94e0ab03da1f76189ea09
yanyuxin <617702281@qq.com>
2020-09-21 23:21:23 +0800

    find finish

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,7 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const max = 2020 - 2000;
+  const min = 2020 - 2010;
+  const result = collection.find(item => item.age >= min && item.age < max);
+  return result.name;
 }
