47dbbbc59224723f48b5bbe2d829101017182ac9
yanyuxin <617702281@qq.com>
2020-09-21 22:22:39 +0800

    startsWith finish

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,10 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  var num = 0;
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].substr(0, 2) == '粤A') {
+      num++;
+    }
+  }
+  return num;
 }
