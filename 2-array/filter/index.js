c719907781d756947c2b1ff96b8f62cdfcf9f3d1
yanyuxin <617702281@qq.com>
2020-09-21 23:13:05 +0800

    filter finish

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,14 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  var result = collection.filter(item => item % 3 === 0);
+  return result;
 }
 
 function chooseNoRepeatNumber(collection) {
-  // TODO 2: 在这里写实现代码
+  // TODO 2: 在这里写实现代码 写了一个过滤器
+  return collection.filter(function(element, index, self) {
+    return self.indexOf(element) === index;
+  });
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
