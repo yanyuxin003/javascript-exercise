1126d1cfa6a572292b73cedbe015c7b31a3cd933
yanyuxin <617702281@qq.com>
2020-09-22 08:08:51 +0800

    assign finish

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,11 @@
 export default function addSerialNumber(source) {
-  // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign 方法用于将所有可枚举属性的值从一个或多个源对象复制到目标对象。
+  const target = {
+    serialNumber: '12345',
+    properties: {
+      color: 'green',
+      status: 'processed'
+    }
+  };
+  return Object.assign(source, target);
 }
