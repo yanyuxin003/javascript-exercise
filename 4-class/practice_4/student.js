989aa78abe5f62df701079f9a1a754f4d70ea15e
yanyuxin <617702281@qq.com>
2020-09-22 08:31:35 +0800

    practice_4 finish

--- 4-class/practice_4/student.js
+++ 4-class/practice_4/student.js
@@ -1,1 +1,13 @@
 // TODO 16: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.`;
+  }
+}
--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,12 @@
 // TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old. I am a Worker. I have a job.`;
+  }
+}
