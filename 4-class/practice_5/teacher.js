c8e5ca5d6c1e72a37631d033c73599aec57e86de
yanyuxin <617702281@qq.com>
2020-09-22 08:36:19 +0800

    practice_5 finish

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,16 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    if (this.klass) {
+      return `${super.introduce()} I am a Teacher. I teach Class ${this.klass}.`;
+    }
+    return `${super.introduce()} I am a Teacher. I teach No Class.`;
+  }
+}
