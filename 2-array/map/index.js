8c1335cd5263759ab153651ef022bb88bd36d378
yanyuxin <617702281@qq.com>
2020-09-22 08:05:51 +0800

    map finish

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,4 @@
 export default function numberMapToWord(collection) {
-  // TODO 3: 在这里写实现代码
+  // TODO 3: 在这里写实现代码  String.fromCharCode就是由编码转为字母的
+  return collection.map(item => String.fromCharCode(item + 96));
 }
