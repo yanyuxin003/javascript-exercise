c7a2e150c35f24415e510481654728e55dcf3cf3
yanyuxin <617702281@qq.com>
2020-09-22 08:14:53 +0800

    values finish

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,9 @@
 export default function countTypesNumber(source) {
-  // TODO 6: 在这里写实现代码
+  // TODO 6: 在这里写实现代码  parseInt()是把String变成int的基础数据类型；
+  let sum = 0;
+  let value = Object.values(source);
+  for (let i = 0; i < value.length; i++) {
+    sum += parseInt(values[i]);
+  }
+  return sum;
 }
